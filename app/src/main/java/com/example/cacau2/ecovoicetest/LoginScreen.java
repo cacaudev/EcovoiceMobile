25a2a0bc7f682e415830985dba53e4b6f826c095
cacaudev <cacauabreu2013@gmail.com>
2018-04-25 21:01:41 -0300

    Tela Cadastro de Conta consertada

--- app/src/main/java/com/example/cacau2/ecovoicetest/LoginScreen.java
+++ app/src/main/java/com/example/cacau2/ecovoicetest/LoginScreen.java
@@ -3,19 +3,62 @@ package com.example.cacau2.ecovoicetest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements OnClickListener {
+
+    public TextView email_text, password_text, forgot_password;
+    public EditText email_field, password_field;
+    public Button create_account, access, registerTree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setTitle("Login"); //Muda nome da barra superior da activity
         setContentView(R.layout.activity_login_screen);
+
+        email_text = findViewById(R.id.login_email_text);
+        password_text = findViewById(R.id.login_password_text);
+
+        email_field = findViewById(R.id.login_email_field);
+        password_field = findViewById(R.id.login_password_field);
+
+        create_account = findViewById(R.id.login_create_account_button);
+        create_account.setOnClickListener(this);
+        access = findViewById(R.id.login_access_button);
+        access.setOnClickListener(this);
+        forgot_password = findViewById(R.id.login_forgot_password_text);
+        forgot_password.setOnClickListener(this);
+        registerTree = findViewById(R.id.login_cadastrar_arvore);
+        registerTree.setOnClickListener(this);
     }
 
-    public void cadastraArvore(View v){
-        Intent it = new Intent(getBaseContext(), Cadastro_de_Arvore.class);
-        startActivity(it);
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.login_access_button:
+                Toast.makeText(getApplicationContext(),"Tela Feed ainda não foi implementada.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.login_create_account_button:
+                Intent register_screen = new Intent(getBaseContext(), RegisterScreen.class);
+                startActivity(register_screen);
+                finish();
+                break;
+            case R.id.login_forgot_password_text:
+                Toast.makeText(getApplicationContext(),"Tela Mandar Senha ainda não foi implementada.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.login_cadastrar_arvore:
+                Intent it = new Intent(getBaseContext(), Cadastro_de_Arvore.class);
+                startActivity(it);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/cacau2/ecovoicetest/RegisterScreen.java
+++ app/src/main/java/com/example/cacau2/ecovoicetest/RegisterScreen.java
@@ -1,14 +1,42 @@
 package com.example.cacau2.ecovoicetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class RegisterScreen extends AppCompatActivity {
+public class RegisterScreen extends AppCompatActivity implements OnClickListener {
+
+    public Button add_account, already_have_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setTitle("Register"); //Muda nome da barra superior da activity
         setContentView(R.layout.activity_register_screen);
+
+        add_account = findViewById(R.id.register_button);
+        add_account.setOnClickListener(this);
+        already_have_account = findViewById(R.id.register_back_to_login_button);
+        already_have_account.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.register_button:
+                Toast.makeText(getApplicationContext(), "Tela Feed ainda não foi implementada.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.register_back_to_login_button:
+                Intent login_screen = new Intent(getBaseContext(), LoginScreen.class);
+                startActivity(login_screen);
+                finish();
+                break;
+        }
     }
 }
+
